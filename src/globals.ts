860ff5e6e1bc92bd50643e31ffa815665bc6b3e4
Jarrod Brandreth <jarrod.brandreth@gmail.com>
2023-02-09 10:17:50 +0100

    increase bounds

--- src/globals.ts
+++ src/globals.ts
@@ -1,6 +1,6 @@
 const latitudeMin = 40.439284;
 const latitudeMax = 40.976443;
 const longitudeMin = -74.384081;
-const longitudeMax = -73.792193;
+const longitudeMax = -73.686260;
 
 export { latitudeMin, latitudeMax, longitudeMin, longitudeMax };
